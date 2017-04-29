b59c6174a41228e92d1a0131a0fa8f1516bdc88f
Tao Gong <tao.gong@uconn.edu>
2017-04-29 14:01:22 -0400

    Tail_tau 0 check fixed Collective2Tail feedforward added

--- flight/modules/Stabilization/obar.c
+++ flight/modules/Stabilization/obar.c
@@ -2,6 +2,7 @@
 #include <pios_math.h>
 #include "stabilization.h"
 #include "stabilizationsettings.h"
+#include "manualcontrolcommand.h"
 
 typedef struct{
 	int a;
@@ -31,22 +32,31 @@ static float stabilization_obar_cyclic(float command, float gyro, float dT, bool
     float decay_alpha = tau_to_alpha(stabSettings.settings.OBarCyclic.Tau);
     stabSettings.innerPids[axis].iAccumulator*=decay_alpha;
     float setpoint_weight = stabSettings.settings.OBarCyclic.SetpointWeight;
-    float output_forward = command * setpoint_weight;
-    float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], &scaler_1, (command - output_forward) * rate,
+    float command_forward = command * setpoint_weight;
+    float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], &scaler_1, (command - command_forward) * rate,
          gyro, dT, measuredDterm_enabled);
-    return output_forward + output_back;
+    return command_forward + output_back;
 }
 
 static float stabilization_obar_tail(float command, float gyro, float dT, bool measuredDterm_enabled, bool reinit, uint32_t axis)
 {
     (void)reinit;
     float rate=StabilizationBankManualRateToArray(stabSettings.stabBank.ManualRate)[axis];
-    float decay_alpha = tau_to_alpha(stabSettings.settings.OBarTail.Tau);
-    stabSettings.innerPids[axis].iAccumulator*=decay_alpha;
+    if(stabSettings.settings.OBarTail.Tau > 0.0001f) {
+    //Tau is TIME CONSTANT in second, however we use 0 to disable it
+        float decay_alpha = tau_to_alpha(stabSettings.settings.OBarTail.Tau);
+        stabSettings.innerPids[axis].iAccumulator*=decay_alpha;
+    }
     float setpoint_weight = stabSettings.settings.OBarTail.SetpointWeight;
-    float output_forward = command * setpoint_weight;
-    float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], &scaler_1, (command - output_forward) * rate, gyro, dT, measuredDterm_enabled);
-    return output_forward + output_back;
+
+    float command_forward = command * setpoint_weight;
+
+    float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], &scaler_1, (command - command_forward) * rate, gyro, dT, measuredDterm_enabled);
+
+    float collective;
+    ManualControlCommandCollectiveGet(&collective);
+    float output_forward = fabsf(collective) * stabSettings.settings.OBarFeedforward.Collective2Tail;
+    return output_forward + output_back + command_forward;
 }
 
 float stabilization_obar_axes(float command, float gyro, float dT, bool measuredDterm_enabled, bool reinit, uint32_t axis)
