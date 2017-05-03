9c25db5680342d1c6f94dc4f345e6821a9c4098d
Tao Gong <tao.gong@uconn.edu>
2017-05-03 02:56:27 -0400

    Tail algorithm fine changed. Stack size increased another bit.

--- flight/modules/Stabilization/obar.c
+++ flight/modules/Stabilization/obar.c
@@ -5,17 +5,20 @@
 #include "manualcontrolcommand.h"
 #include <pios_rpm.h>
 
-static inline float low_pass_filter(float*history, float value, float alpha)
+static struct pid governorPid;
+static const pid_scaler scaler_111 = {.p=1.0f, .i=1.0f, .d=1.0f,};
+
+static inline void obar_axes_init(uint32_t axis)
 {
-    return (*history=(*history)*(1-alpha)+value*alpha);
+    pid_zero(&stabSettings.innerPids[axis]);
+}
+static inline void obar_governor_init()
+{
+    pid_zero(&governorPid);
 }
 
-static const pid_scaler scaler_111 = {.p=1.0f, .i=1.0f, .d=1.0f,};
-static const pid_scaler scaler_101 = {.p=1.0f, .i=0.0f, .d=1.0f,};
-
-static inline float stabilization_obar_cyclic(float command, float gyro, float dT, bool measuredDterm_enabled, bool reinit, uint32_t axis)
+static inline float obar_cyclic(float command, float gyro, float dT, bool measuredDterm_enabled, uint32_t axis)
 {
-    (void)reinit;
     float rate=StabilizationBankManualRateToArray(stabSettings.stabBank.ManualRate)[axis];
     stabSettings.innerPids[axis].iAccumulator*=stabSettings.settings.OBarCyclic.Decay;
     float setpoint_weight = stabSettings.settings.OBarCyclic.SetpointWeight;
@@ -24,63 +27,95 @@ static inline float stabilization_obar_cyclic(float command, float gyro, float d
          gyro, dT, measuredDterm_enabled);
     return command_forward + output_back;
 }
-
-static inline float stabilization_obar_tail(float command, float gyro, float dT, bool measuredDterm_enabled, bool reinit, uint32_t axis)
+static inline float tail_prefilter(float command, float dT)
 {
-    (void)reinit;
-    const pid_scaler * p_scaler = &scaler_111;
-    static float command_lpf=0;
-    low_pass_filter(&command_lpf, command, stabSettings.settings.OBarTail.LowPassFilter);
-    if (fabsf(command_lpf-command)>stabSettings.settings.OBarExtra.A){
-        //damping, don't accumulate I
-	p_scaler = &scaler_101;
+    StabilizationSettingsOBarTailData *const obartail = &stabSettings.settings.OBarTail;
+    static float command0 = 0;
+    static float command_rate0 = 0;
+
+    //rate cap; value in unit degree/sec;
+    //90degree = [-1,+1]. Angle = command * 45
+    // (command_rate * 45) = degree/sec
+    float command_rate = (command - command0) /dT;
+    const float max_command_rate = obartail->ServoRateLimit / 45;
+    if( command_rate > max_command_rate){
+        command_rate = max_command_rate;
     }
-    command = command_lpf;
+    if( command_rate < -max_command_rate){
+        command_rate = -max_command_rate;
+    }
+
+    //accel cap; value in unit degree/sec^2;
+    // (command_diff_diff * 45)/dT = degree/sec^2
+    //just cap the accel, don't cap decel (not necessary)
+    float command_accel = (command_rate - command_rate0) / dT;
+    const float max_command_accel = obartail->ServoAccelLimit / 45;
+    //filter out deceleration:
+    // rate and rate0 has same direction and |rate| < |rate0|
+    if(fabsf(command_rate) > fabsf(command_rate0) || command_rate * command_rate0 < 0) {
+        if( command_accel > max_command_accel){
+            command_accel = max_command_accel;
+        }
+        if( command_accel < -max_command_accel){
+            command_accel = -max_command_accel;
+        }
+    }
+    command_rate = command_rate0 + command_accel * dT;
+    command = command0 + command_rate * dT;
+    command0 = command;
+    command_rate0 = command_rate;
+
+    return command;
+}
+static inline float obar_tail(float command, float gyro, float dT, bool measuredDterm_enabled, uint32_t axis)
+{
+    command = tail_prefilter(command, dT);
+    StabilizationSettingsOBarTailData *const obartail = &stabSettings.settings.OBarTail;
+
     float rate=StabilizationBankManualRateToArray(stabSettings.stabBank.ManualRate)[axis];
-    stabSettings.innerPids[axis].iAccumulator*=stabSettings.settings.OBarTail.Decay;
-    float setpoint_weight = stabSettings.settings.OBarTail.SetpointWeight;
+    stabSettings.innerPids[axis].iAccumulator*=obartail->Decay;
+    float setpoint_weight = obartail->SetpointWeight;
 
     float command_forward = command * setpoint_weight;
 
-    float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], p_scaler, (command - command_forward) * rate, gyro, dT, measuredDterm_enabled);
+    float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], &scaler_111, (command - command_forward) * rate, gyro, dT, measuredDterm_enabled);
 
     float collective, roll, pitch;
     ManualControlCommandCollectiveGet(&collective);
     ManualControlCommandRollGet(&roll);
     ManualControlCommandPitchGet(&pitch);
-    float output_forward = fabsf(collective) * stabSettings.settings.OBarTail.CollectiveFeedforward;
-    output_forward += (fabsf(roll) + fabsf(pitch)) * stabSettings.settings.OBarTail.CyclicFeedforward;
+    float output_forward = fabsf(collective) * obartail->CollectiveFeedforward;
+    output_forward += (fabsf(roll) + fabsf(pitch)) * obartail->CyclicFeedforward;
     return output_forward + output_back + command_forward;
 }
 
 float stabilization_obar_axes(float command, float gyro, float dT, bool measuredDterm_enabled, bool reinit, uint32_t axis)
 {
+    if (reinit) {
+        obar_axes_init(axis);
+    }
     switch (axis) {
     case 0:
     case 1:
-        return stabilization_obar_cyclic(command, gyro, dT, measuredDterm_enabled, reinit, axis);
+        return obar_cyclic(command, gyro, dT, measuredDterm_enabled, axis);
     case 2:
-        return stabilization_obar_tail(command, gyro, dT, measuredDterm_enabled, reinit, axis);
+        return obar_tail(command, gyro, dT, measuredDterm_enabled, axis);
     default:
         return 0;
     }
 }
 
-static struct pid governorPid;
-void stabilization_obar_init()
-{
-    pid_zero(&governorPid);
-}
-
 float stabilization_obar_governor(float command, float dT, bool measuredDterm_enabled, bool reinit)
 {
-    (void)reinit;
+    if (reinit) {
+        obar_governor_init();
+    }
     float currentRpm = PIOS_RPM_GetRPM();
+    //safe mode either throttle < 50% or RPM reads out < 100
     if( command < 0.5f || currentRpm < 100.0f) return command;
-    StabilizationSettingsOBarGovernorData *obargovernor = &stabSettings.settings.OBarGovernor;
+    StabilizationSettingsOBarGovernorData *const obargovernor = &stabSettings.settings.OBarGovernor;
     pid_configure(&governorPid, obargovernor->Kp, obargovernor->Ki, obargovernor->Kd, 1.0f);
     float desiredRpm = obargovernor->HeadSpeed * obargovernor->GearRatio * obargovernor->SensorScale;
-
     float output = command + pid_apply_setpoint(&governorPid, &scaler_111, desiredRpm, currentRpm, dT, measuredDterm_enabled);
     return output;
 }
