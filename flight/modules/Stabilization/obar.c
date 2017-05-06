6ed0e56f8fda480254d77cf48f99602100b79d11
Tao Gong <tao.gong@uconn.edu>
2017-05-06 19:49:45 -0400

    Tail algorithm optimized

--- flight/modules/Stabilization/obar.c
+++ flight/modules/Stabilization/obar.c
@@ -4,6 +4,7 @@
 #include "stabilizationsettings.h"
 #include "manualcontrolcommand.h"
 #include <pios_rpm.h>
+#include <pios_led.h>
 
 static struct pid governorPid;
 static const pid_scaler scaler_111 = {.p=1.0f, .i=1.0f, .d=1.0f,};
@@ -27,6 +28,57 @@ static inline float obar_cyclic(float command, float gyro, float dT, bool measur
          gyro, dT, measuredDterm_enabled);
     return command_forward + output_back;
 }
+static inline bool detect_saturation(float output, float dT)
+{
+    StabilizationSettingsOBarTailData *const obartail = &stabSettings.settings.OBarTail;
+    static float output0 = 0;
+    static float output_rate0 = 0;
+    bool detected = false;
+
+    //rate cap; value in unit degree/sec;
+    //90degree = [-1,+1]. Angle = output * 45
+    // (output_rate * 45) = degree/sec
+    float output_rate = (output - output0) /dT;
+    const float max_output_rate = obartail->ServoRateLimit / 45;
+    if( output_rate > max_output_rate){
+            output_rate = max_output_rate;
+	    detected = true;
+    }
+    if( output_rate < -max_output_rate){
+            output_rate = -max_output_rate;
+	    detected = true;
+    }
+
+    
+    //accel cap; value in unit degree/sec^2;
+    // (output_diff_diff * 45)/dT = degree/sec^2
+    //just cap the accel, don't cap decel (not necessary)
+    float output_accel = (output_rate - output_rate0) / dT;
+    const float max_output_accel = obartail->ServoAccelLimit / 45;
+    //filter out deceleration:
+    // rate and rate0 has same direction and |rate| < |rate0|
+    if(fabsf(output_rate) > fabsf(output_rate0) || output_rate * output_rate0 < 0) {
+        if( output_accel > max_output_accel){
+            output_accel = max_output_accel;
+	    detected = true;
+        }
+        if( output_accel < -max_output_accel){
+            output_accel = -max_output_accel;
+	    detected = true;
+        }
+    }
+    
+    //however, we only store the bounded output here statically. 
+    //It doesn't apply to the real output.
+    //Which is good, so user could always get fast response.
+    output_rate = output_rate0 + output_accel * dT;
+    output_rate0 = output_rate;
+    output = output0 + output_rate * dT;
+    output0 = output;
+
+    return detected;
+}
+/*
 static inline float tail_prefilter(float command, float dT)
 {
     StabilizationSettingsOBarTailData *const obartail = &stabSettings.settings.OBarTail;
@@ -67,15 +119,20 @@ static inline float tail_prefilter(float command, float dT)
 
     return command;
 }
+*/
 static inline float obar_tail(float command, float gyro, float dT, bool measuredDterm_enabled, uint32_t axis)
 {
-    command = tail_prefilter(command, dT);
     StabilizationSettingsOBarTailData *const obartail = &stabSettings.settings.OBarTail;
 
     float rate=StabilizationBankManualRateToArray(stabSettings.stabBank.ManualRate)[axis];
-    stabSettings.innerPids[axis].iAccumulator*=obartail->Decay;
-    float setpoint_weight = obartail->SetpointWeight;
 
+    //apply decay
+    stabSettings.innerPids[axis].iAccumulator*=obartail->Decay;
+
+    //store old integral in case of saturation detected
+    float iAccumulator0 = stabSettings.innerPids[axis].iAccumulator;
+
+    float setpoint_weight = obartail->SetpointWeight;
     float command_forward = command * setpoint_weight;
 
     float output_back = pid_apply_setpoint(&stabSettings.innerPids[axis], &scaler_111, (command - command_forward) * rate, gyro, dT, measuredDterm_enabled);
@@ -86,7 +143,16 @@ static inline float obar_tail(float command, float gyro, float dT, bool measured
     ManualControlCommandPitchGet(&pitch);
     float output_forward = fabsf(collective) * obartail->CollectiveFeedforward;
     output_forward += (fabsf(roll) + fabsf(pitch)) * obartail->CyclicFeedforward;
-    return output_forward + output_back + command_forward;
+    float output = output_forward + output_back + command_forward;
+
+    //anti-windup
+    if(detect_saturation(output,dT)){
+        stabSettings.innerPids[axis].iAccumulator = iAccumulator0;
+        PIOS_LED_On(PIOS_LED_HEARTBEAT);
+    } else {
+        PIOS_LED_Off(PIOS_LED_HEARTBEAT);
+    }
+    return output;
 }
 
 float stabilization_obar_axes(float command, float gyro, float dT, bool measuredDterm_enabled, bool reinit, uint32_t axis)
